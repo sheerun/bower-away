be890213b83ea81f3d65a31f197e26c74ae43c91
Adam Stankiewicz <sheerun@sher.pl>
2017-10-19 23:28:20 +0200

    Automatically add postinstall script with link

--- packages/bower-away/cli.js
+++ packages/bower-away/cli.js
@@ -25,6 +25,19 @@ const cli = meow(
   `
 )
 
+const lastMessage = [
+  'Your project is now converted to Yarn! Thank you for using Bower!',
+  '',
+  'You should find all bower components in node_modules/@bower_components',
+  '',
+  'The postinstall script should also link it to old location of components',
+  '',
+  'It is advisable to remove postinstall script and point your tools',
+  'to point to node_modules/@bower_components instead, though.',
+  '',
+  'You may also consider creating separate directory for front-end project with separate package.json'
+]
+
 function help () {
   console.error(cli.help)
   process.exit(1)
@@ -66,11 +79,7 @@ async function main () {
 
   if (!fs.existsSync(path.join(cwd, 'bower.json'))) {
     if (fs.existsSync(path.join(cwd, 'node_modules', '@bower_components'))) {
-      step(
-        'Done',
-        ['Your project is now converted to Yarn! Thank you for using Bower!'],
-        true
-      )
+      step('Done', lastMessage, true)
     } else {
       step('Browse to project directory', [
         'Current directory does not contain bower.json',
@@ -119,18 +128,13 @@ async function main () {
   const config = bowerConfig.read(cwd)
   const componentsDir = path.resolve(cwd, config.directory)
 
-  if (
-    !fs.existsSync(componentsDir) &&
-    !fs.existsSync(path.join(cwd, 'node_modules', '@bower_components'))
-  ) {
-    step('Install dependencies with Bower', [
-      'We need to install dependencies the old way first. Please run:',
-      '$ bower install',
-      '',
-      'At the end you should see some packages in:',
-      componentsDir
-    ])
-  }
+  let hasComponentsDir = false
+
+  try {
+    if (fs.lstatSync(componentsDir).isDirectory()) {
+      hasComponentsDir = true
+    }
+  } catch (e) {}
 
   let original = {}
 
@@ -140,113 +144,149 @@ async function main () {
 
   const expected = cloneDeep(original)
 
+  let hadBowerComponents = false
+
   if (expected.dependencies) {
     Object.keys(expected.dependencies).forEach(function (k) {
       if (k.indexOf('@bower_components') >= 0) {
+        hadBowerComponents = true
         delete expected.dependencies[k]
       }
     })
   }
 
-  const pkgs = fs.readdirSync(componentsDir)
-
-  for (const pkg of pkgs) {
-    const manifest = JSON.parse(
-      fs.readFileSync(path.join(componentsDir, pkg, '.bower.json'))
-    )
-
-    let source = manifest._source
-    let target = manifest._target
-
-    if (source.slice(0, 19) === 'https://github.com/') {
-      source = source.slice(19)
-
-      if (source.slice(-4) === '.git') {
-        source = source.slice(0, -4)
-      }
-
-      source = source
-    }
-
-    if (!('dependencies' in expected)) {
-      expected.dependencies = {}
-    }
-
-    expected.dependencies[`@bower_components/${pkg}`] = `${source}#${target}`
-  }
-
-  if (typeof expected.engines !== 'object') {
-    expected.engines = {}
-  }
-
-  if (!expected.engines.yarn) {
-    expected.engines.yarn = '>= 1.0.0'
-  }
-
-  if (!deepIs(expected, original)) {
-    if (cli.flags.diff) {
-      if (process.stdout.isTTY) {
-        console.log(difflet.compare(original, expected, { indent: 2 }))
-      }
-
-      process.exit(0)
-    } else if (cli.flags.apply) {
-      fs.writeFileSync('package.json', JSON.stringify(expected, null, 2) + '\n')
-    } else {
-      if (fs.existsSync('package.json')) {
-        step('Update package.json', [
-          'Changes need to be made in package.json. Please run following to preview them:',
-          '',
-          '$ bower-away --diff',
-          '',
-          'And then apply them by running:',
-          '',
-          '$ bower-away --apply'
-        ])
-      }
-    }
-  }
-
-  if (!fs.existsSync(path.join(cwd, 'node_modules', '@bower_components'))) {
-    step('Install dependencies with Yarn', [
-      'Now what package.json contains Bower dependencies, please install them with:',
-      '$ yarn',
+  if (!hasComponentsDir && !hadBowerComponents) {
+    step('Install dependencies with Bower', [
+      'We need to install dependencies the old way first. Please run:',
+      '$ bower install',
       '',
-      'If you encounter issues during installation, please try:',
-      '$ yarn --ignore-engines --ignore-scripts',
-      '',
-      'You can use this command from now on to install both npm and bower dependencies.'
+      'At the end you should see some packages in:',
+      componentsDir
     ])
   }
 
   const source = path.relative(process.cwd(), componentsDir)
-  const target = path.relative(
-    path.dirname(componentsDir),
-    'node_modules/@bower_components'
-  )
-  const script = 'rm -rf ' + source + ' && ln -sf ' + target + ' ' + source
 
-  if (fs.existsSync(path.join(cwd, 'bower.json'))) {
-    step('Remove bower.json and old bower components directory', [
-      'Your project is now converted to Yarn!',
-      '',
-      'You should find all bower components in node_modules/@bower_components',
-      '',
-      'If you would like them to be available in previous location',
-      'please consider adding following as postinstall script in package.json:',
-      '',
-      '"postinstall": "' + script + '"',
-      '',
-      'As a last step, please remove bower.json, .bowerrc',
-      'and point your scripts to node_modules/@bower_components instead'
+  if (hasComponentsDir) {
+    const pkgs = fs.readdirSync(componentsDir)
+
+    for (const pkg of pkgs) {
+      const manifest = JSON.parse(
+        fs.readFileSync(path.join(componentsDir, pkg, '.bower.json'))
+      )
+
+      let source = manifest._source
+      let target = manifest._target
+
+      if (source.slice(0, 19) === 'https://github.com/') {
+        source = source.slice(19)
+
+        if (source.slice(-4) === '.git') {
+          source = source.slice(0, -4)
+        }
+
+        source = source
+      }
+
+      if (!('dependencies' in expected)) {
+        expected.dependencies = {}
+      }
+
+      expected.dependencies[`@bower_components/${pkg}`] = `${source}#${target}`
+    }
+
+    if (typeof expected.engines !== 'object') {
+      expected.engines = {}
+    }
+
+    if (!expected.engines.yarn) {
+      expected.engines.yarn = '>= 1.0.0'
+    }
+
+    const script =
+      "node -e \"try { require('fs').symlinkSync(require('path').resolve('node_modules/@bower_components'), '" +
+      source +
+      "', 'junction') } catch (e) { }\""
+
+    if (!expected.scripts) {
+      expected.scripts = {}
+    }
+
+    if (
+      !expected.scripts.postinstall ||
+      expected.scripts.postinstall.indexOf('bower install') >= 0
+    ) {
+      expected.scripts.postinstall = script
+    } else if (expected.scripts.postinstall.indexOf('symlinkSync') == -1) {
+      expected.scripts.postinstall =
+        expected.scripts.postinstall + ' && ' + script
+    }
+
+    if (!deepIs(expected, original)) {
+      if (cli.flags.diff) {
+        if (process.stdout.isTTY) {
+          console.log(difflet.compare(original, expected, { indent: 2 }))
+        }
+
+        process.exit(0)
+      } else if (cli.flags.apply) {
+        fs.writeFileSync(
+          'package.json',
+          JSON.stringify(expected, null, 2) + '\n'
+        )
+      } else {
+        if (fs.existsSync('package.json')) {
+          step('Update package.json', [
+            'Changes need to be made in package.json. Please run following to preview them:',
+            '',
+            '$ bower-away --diff',
+            '',
+            'And then apply them by running:',
+            '',
+            '$ bower-away --apply'
+          ])
+        }
+      }
+    }
+  }
+
+  let oldDirExists = false
+
+  try {
+    if (fs.lstatSync(componentsDir).isDirectory()) {
+      oldDirExists = true
+    }
+  } catch (e) {}
+
+  if (oldDirExists) {
+    step('Remove old components directory', [
+      'Now, please remove original components directory:',
+      '$ rm -rf ' + source
     ])
   }
 
-  step(
-    'Done',
-    ['Your project is now converted to Yarn! Thank you for using Bower!'],
-    true
-  )
+  if (!fs.existsSync(path.join(cwd, 'node_modules', '@bower_components'))) {
+    step('Install dependencies with Yarn', [
+      'Now install dependencies again with:',
+      '$ yarn',
+      '',
+      'If you encounter issues during installation, please try:',
+      '$ yarn --ignore-engines',
+      '',
+      'If it also fails, you can try following:',
+      '$ yarn --ignore-engines --ignore-scripts && yarn postinstall',
+      '',
+      'You can use this command from now on to install both npm and bower dependencies!'
+    ])
+  }
+
+  if (fs.existsSync(path.join(cwd, 'bower.json'))) {
+    step('Remove bower.json and old bower components directory', [
+      'As a last step, please remove bower.json and .bowerrc'
+    ])
+  }
+
+  step('Done', lastMessage, true)
 }
 
 main()
